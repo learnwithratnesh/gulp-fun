d205118bf99b6b83a4de12077dca73703e4cbe7d
Ratnesh Tiwari <ratneshtiwari@Ratneshs-MacBook-Air.local>
2020-11-20 10:26:58 +0530

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+gulp.task('sass', function() {
+    return gulp.src('src/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+      .pipe(sass())
+      .pipe(gulp.dest('dist/app/css'),{append:true})
+      .pipe(concat('output.css'))
+  })
+
+  gulp.task('watch', function(){
+    gulp.watch('src/scss/**/*.scss', ['sass']); 
+    // Other watchers
+  })
